5bcf2e325d88ee97b25f31486561cb262699dc0b
zhangjunyang <95231885@qq.com>
2018-12-29 16:12:50 +0800

    jdbc操作类

--- src/main/java/jdbc/JdbcTest.java
+++ src/main/java/jdbc/JdbcTest.java
@@ -7,6 +7,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @author ZJY
@@ -15,8 +17,14 @@ import java.util.Random;
  * @date 2018/12/28 14:56
  */
 public class JdbcTest {
+    // 十个线程的线程池
+    private static ExecutorService executor = Executors.newFixedThreadPool(10);
+    // 准备数据的数量
+    private static final int prepareCount = 60000;
+    // 累加器
+    private volatile int count = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         long start = new Date().getTime();
         String memorySql = "SELECT\n" +
                 "\t id taskId, task_status, modality, patient_name, accession_no,\n" +
@@ -25,7 +33,7 @@ public class JdbcTest {
                 "\t review_doctor_id, update_time, study_datetime, from_org_no,\n" +
                 "\t to_org_no, task_level, create_time \n" +
                 "FROM\n" +
-                "\t dw_doctor_task\n" +
+                "\t dw_doctor_task_memory\n" +
                 "WHERE 1=1\n" +
                 "\t AND task_level = 0 \n" +
                 "\t AND report_doctor_id IS NULL \n" +
@@ -60,6 +68,12 @@ public class JdbcTest {
         start = new Date().getTime();
         queryWithSql(innodbSql);
         System.out.println("innodb engine查询100次耗时：" + (new Date().getTime() - start));
+
+//        JdbcTest test = new JdbcTest();
+//        PrepareDataTask task = test.new PrepareDataTask();
+//        for (int i = 0; i < 10; i++) {
+//            executor.submit(task);
+//        }
     }
 
     private static void queryWithSql(String sql) {
@@ -85,38 +99,41 @@ public class JdbcTest {
         }
     }
 
-    /**
-     * @function 插入给定数量的数据
-     * @author ZJY 2018/12/28 20:49
-     */
-    private static void prepareData(int num) throws Exception {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://39.105.8.99/ris", "root", "Zjy12345+");
-        PreparedStatement pstmt = conn.prepareStatement("insert into dw_doctor_task ( patient_id, study_iuid, patient_sex, from_org_no, to_org_no, " +
-                "create_time, update_time ) values( ?, ?, ?, ?, ?, ?, ? )");
+    class PrepareDataTask implements Runnable {
+        @Override
+        public void run() {
+            try {
+                Connection conn = DriverManager.getConnection("jdbc:mysql://39.105.8.99/ris", "root", "Zjy12345+");
+                PreparedStatement pstmt = conn.prepareStatement("insert into dw_doctor_task_memory ( patient_id, study_iuid, patient_sex, from_org_no, to_org_no, " +
+                        "create_time, update_time ) values( ?, ?, ?, ?, ?, ?, ? )");
+                Random random = new Random();
+                while (count < prepareCount) {
+                    String patientId = "patient" + random.nextInt(40000);
+                    String studyUid = "studyUid" + random.nextInt(40000);
+                    String patientSex = "M";
+                    String orgNo = "sr_test_0001_001";
+                    Date createTime = getRandomDate("2018-11-11", "2018-11-13");
+                    Date updateTime = createTime;
 
-        Random random = new Random();
+                    pstmt.setString(1, patientId);
+                    pstmt.setString(2, studyUid);
+                    pstmt.setString(3, patientSex);
+                    pstmt.setString(4, orgNo);
+                    pstmt.setString(5, orgNo);
+                    pstmt.setTimestamp(6, new java.sql.Timestamp(createTime.getTime()));
+                    pstmt.setTimestamp(7, new java.sql.Timestamp(updateTime.getTime()));
+                    try {
+                        pstmt.execute();
+                    } catch (Exception e) {}
 
-        for (int i = 0; i < num; i ++) {
-            String patientId = "patient" + random.nextInt(10000);
-            String studyUid = "studyUid" + random.nextInt(10000);
-            String patientSex = "M";
-            String orgNo = "sr_test_0001_001";
-            Date createTime = getRandomDate("2018-11-11", "2018-11-13");
-            Date updateTime = createTime;
-
-            pstmt.setString(1, patientId);
-            pstmt.setString(2, studyUid);
-            pstmt.setString(3, patientSex);
-            pstmt.setString(4, orgNo);
-            pstmt.setString(5, orgNo);
-            pstmt.setTimestamp(6, new java.sql.Timestamp(createTime.getTime()));
-            pstmt.setTimestamp(7, new java.sql.Timestamp(updateTime.getTime()));
-
-            pstmt.execute();
+                    count ++;
+                }
+                pstmt.close();
+                conn.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
         }
-
-        pstmt.close();
-        conn.close();
     }
 
     /**
