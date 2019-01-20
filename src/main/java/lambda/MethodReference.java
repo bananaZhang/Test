702e83ebb067860312f4c91d3729f5223a2d576c
bananaZhang <jyzhang1994@163.com>
2019-01-20 21:10:27 +0800

    方法引用的三种方式

--- src/main/java/lambda/MethodReference.java
+++ src/main/java/lambda/MethodReference.java
@@ -1,8 +1,9 @@
 package lambda;
 
-import java.util.List;
+import java.io.PrintStream;
+import java.util.Comparator;
 import java.util.function.BiPredicate;
-import java.util.function.Function;
+import java.util.function.Consumer;
 
 /**
  * 方法引用的三种方式
@@ -12,12 +13,18 @@ import java.util.function.Function;
 public class MethodReference {
 
     public static void main(String[] args) {
-        // 第一种：指向静态方法的方法引用
-        Function<String, Integer> function1 = Integer::parseInt;
-        // 第二种：指向任意类型实例方法的方法引用
-        Function<String, Integer> function2 = String::length;
-        // 第三种：指向现有对象的实例方法的方法引用
-        BiPredicate<List<String>, String> function3 = List::contains;
+        // 第一种：指向静态方法的方法引用：类名::静态方法名
+        Comparator<Integer> comparator = Integer::compare;
+        Integer diff = comparator.compare(2, 4);
+        System.out.println("diff: " + diff);
+        // 第二种：指向任意类型实例方法的方法引用：类名::实例方法名
+        BiPredicate<String, String> predicate = String::equals;
+        Boolean isEqual = predicate.test("aaa", "bbb");
+        System.out.println("isEqual: " + isEqual);
+        // 第三种：指向现有对象的实例方法的方法引用：对象的引用::实例方法名
+        PrintStream ps = System.out;
+        Consumer<String> consumer = ps::println;
+        consumer.accept("333");
     }
 
 }
