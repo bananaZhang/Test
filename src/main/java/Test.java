1d15578099b5fd7e8a7d71158f449a008c9cda11
bananaZhang <jyzhang1994@163.com>
2019-03-24 16:33:04 +0800

    测试jstack命令

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,6 @@
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 /**
  * @author ZJY
  * @ClassName: Test
@@ -5,8 +8,30 @@
  * @date 2018/4/21 11:41
  */
 public class Test {
+
+    public static Executor executor = Executors.newFixedThreadPool(5);
+    public static Object lock = new Object();
+
     public static void main(String[] args) {
-        System.out.println(System.getProperty("java.io.tmpdir"));
-        System.out.println(System.getProperty("os.name"));
+        Task task1 = new Task();
+        Task task2 = new Task();
+        executor.execute(task1);
+        executor.execute(task2);
+    }
+
+    static class Task implements Runnable {
+        @Override
+        public void run() {
+            synchronized (lock) {
+                calculate();
+            }
+        }
+
+        public void calculate() {
+            int i = 0;
+            while (true) {
+                i++;
+            }
+        }
     }
 }
