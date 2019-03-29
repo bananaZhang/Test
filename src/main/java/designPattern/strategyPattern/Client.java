849646800fc9db8622367df5c4e13ceb8cab005e
zhangjunyang <95231885@qq.com>
2019-03-29 20:59:54 +0800

    策略模式

--- src/main/java/designPattern/strategyPattern/Client.java
+++ src/main/java/designPattern/strategyPattern/Client.java
@@ -3,7 +3,7 @@ package designPattern.strategyPattern;
 /**
  * @author ZJY
  * @ClassName: Client
- * @Description: Client
+ * @Description: 策略模式在现实中的运用：线程池的拒绝策略
  * @date 2019/3/29 20:41
  */
 public class Client {
--- src/main/java/designPattern/strategyPattern/StrategyContext.java
+++ src/main/java/designPattern/strategyPattern/StrategyContext.java
@@ -6,6 +6,10 @@ package designPattern.strategyPattern;
  * @Description: 策略上下文
  * @date 2019/3/29 20:38
  */
+/**
+ * 在策略模式中，一般情况下都是上下文持有策略的引用，以进行对具体策略的调用。但具体的策略对象也可以从上下文中获取所需数据，
+ * 可以将上下文当做参数传入到具体策略中，具体策略通过回调上下文中的方法来获取其所需要的数据。
+ */
 public class StrategyContext {
 
     private DiscountStrategy discountStrategy;
