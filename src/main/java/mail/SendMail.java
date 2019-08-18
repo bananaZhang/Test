d3e7eaeed9a2594fcfd9c839c9da53884cff83e1
bananaZhang <jyzhang1994@163.com>
2019-08-18 22:19:25 +0800

    Java发送邮件

---
+++ src/main/java/mail/SendMail.java
@@ -0,0 +1,68 @@
+package mail;
+
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
+
+public class SendMail {
+
+    private static final String SMTP_HOST = "smtp.netease.com";
+    private static final String SMTP_PORT = "25";
+
+    public static void main(String[] args) throws Exception {
+        // 1. 创建参数配置, 用于连接邮件服务器的参数配置
+        Properties props = new Properties();                    // 参数配置
+        props.setProperty("mail.transport.protocol", "smtp");   // 使用的协议（JavaMail规范要求）
+        props.setProperty("mail.smtp.host", SMTP_HOST);   // 发件人的邮箱的 SMTP 服务器地址
+        props.setProperty("mail.smtp.port", SMTP_PORT);   // 发件人的邮箱的 SMTP 服务器端口
+//        props.setProperty("mail.smtp.auth", "true");
+
+        // 2. 根据配置创建会话对象, 用于和邮件服务器交互
+        Session session = Session.getInstance(props);
+        session.setDebug(true);                                 // 设置为debug模式, 可以查看详细的发送 log
+
+        // 3. 创建一封邮件
+        MimeMessage message = createMimeMessage(session, "jyzhang1994@163.com", "18328366972@163.com");
+
+        // 4. 根据 Session 获取邮件传输对象
+        Transport transport = session.getTransport();
+
+        // 5. 连接邮件服务器（可设置用户名密码）
+//        transport.connect("uname", "upwd");
+        transport.connect();
+
+        // 6. 发送邮件, 发到所有的收件地址, message.getAllRecipients() 获取到的是在创建邮件对象时添加的所有收件人, 抄送人, 密送人
+        transport.sendMessage(message, message.getAllRecipients());
+
+        // 7. 关闭连接
+        transport.close();
+    }
+
+    private static MimeMessage createMimeMessage(Session session, String sendMail, String receiveMail) throws Exception {
+        // 1. 创建一封邮件
+        MimeMessage message = new MimeMessage(session);
+
+        // 2. From: 发件人
+        message.setFrom(new InternetAddress(sendMail));
+
+        // 3. To: 收件人（可以增加多个收件人、抄送、密送）
+        message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiveMail));
+
+        // 4. Subject: 邮件主题
+        message.setSubject("subject xixi", "UTF-8");
+
+        // 5. Content: 邮件正文（可以使用html标签）
+        message.setContent("data:lala", "text/html;charset=UTF-8");
+
+        // 6. 设置发件时间
+        message.setSentDate(new Date());
+
+        // 7. 保存设置
+        message.saveChanges();
+
+        return message;
+    }
+}
